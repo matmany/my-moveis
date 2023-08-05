ddcd6924bd5d9a12c09123c507fbf1a16b925681
MateusMamani <matmanymat@gmail.com>
2023-08-05 14:59:51 -0300

    feat(migration): cria tabela de movies

---
+++ MyMovieApi/src/MyMovieApi.Infra/Migrations/20230805175633_CreateMovieTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MyMovieApi.Infra.Migrations
+{
+    public partial class CreateMovieTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    imageUrl = table.Column<string>(type: "text", nullable: false),
+                    year = table.Column<string>(type: "text", nullable: false),
+                    synopsis = table.Column<string>(type: "text", nullable: false),
+                    approvalRating = table.Column<string>(type: "text", nullable: false),
+                    dataOrigin = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pK_movies", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "movies");
+        }
+    }
+}
